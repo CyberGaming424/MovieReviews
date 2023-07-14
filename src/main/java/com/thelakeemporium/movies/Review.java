86982946077e1c1eaaeac098994a7a40f73c16c2
BuildTools <jlake624@gmail.com>
2023-07-14 16:56:00 -0400

    Initial commit

---
+++ src/main/java/com/thelakeemporium/movies/Review.java
@@ -0,0 +1,22 @@
+package com.thelakeemporium.movies;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "reviews")
+public class Review {
+
+    @Id
+    private ObjectId id;
+    private String body;
+
+}
